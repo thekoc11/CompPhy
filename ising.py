6219e998b51a77ed63f2da4df3cad40a782c4461
thekoc11 <thekoc11@gmail.com>
2018-11-22 23:20:32 +0530

    plot mag vs temp

--- ising.py
+++ ising.py
@@ -1,7 +1,8 @@
 from numpy import array
 import numpy as np
 import random as rn
-from math import sqrt, ceil, floor, exp
+from math import sqrt, ceil, floor, exp, fabs
+import matplotlib.pyplot as plt
 
 def Energy(s):
     H = 0
@@ -15,27 +16,32 @@ def Energy(s):
             sumAdj = sumAdj + (s[h][j] * s[i][j])
             sumAdj = sumAdj + (s[i][v] * s[i][j])
             v = (v + 1) % L
+            H = H - J * sumAdj * s[i][j]
+
         h = (h + 1) % L
 
-    H = J * sumAdj
     return H
 
-def MonteCarlo(s):
+def MonteCarlo(s, T):
     H = Energy(s)
     s_ = flip(s)
     H_ = Energy(s_)
-
-    print (H_, H)
+    if T > 0:
+        beta = 1 / T
+    else:
+        beta = 100000000000.0
+    # print (H_, H)
     if(H_ <= H):
         s = s_
         H = H_
-        print("flip support")
+        # print("flip support")
     else:
-        acc = exp(-(H_ - H))
+        acc = exp(-beta*(H_ - H))
+        # print (acc, beta)
         if acc > rn.random():
             s = s_
             H = H_
-            print ("accepted")
+            # print ("accepted")
     return s
 
 
@@ -46,7 +52,7 @@ def flip(s):
     for i in range(len(s)):
         sP = []
         for j in range(len(s)):
-            if(i == h) and (j == v):
+            if(i == h) or (j == v):
                 sP.append(-s[i][j])
             else:
                 sP.append(s[i][j])
@@ -68,16 +74,38 @@ for i in range(L):
     sh = array(sh)
     s.append(sh)
 s = array(s)
-print (s)
+# print (s)
 
 U = Energy(s)
 
 print (U)
 
-dt = 0.1
+dt = 0.01
 T = 10
 t = 0
+Temperature = 100.0
+deltaT = 1
 while t<T:
-    s = MonteCarlo(s)
-    print (Energy(s))
+    s = MonteCarlo(s, Temperature)
+    # print (Energy(s))
     t = t + dt
+
+M = np.arange(0, Temperature, deltaT, float)
+
+t = 0
+temp = 0.0
+Y = []
+while temp < Temperature:
+    while t < T:
+        # print ("flow reached here!", temp, t, Temperature)
+        s = MonteCarlo(s, temp)
+        t = t + dt
+    t = 0
+    temp = temp + deltaT
+    S = sum(s)
+    S = fabs(sum(S))/(len(s)*len(s))
+    Y.append(S)
+    # print (S)
+
+plt.plot(M, Y)
+plt.show()
